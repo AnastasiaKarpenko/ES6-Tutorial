aeecc6d11238db3f5a2f90681e81d041849b43d3
Anastasia Karpenko <morganaloca@gmail.com>
2018-12-05 12:21:50 +0200

    Arrow function

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,7 +1,10 @@
 const integers = [1, 2, 3];
 
-const updatedIntegers = integers.map(function (number) {
-    return number += 1
-});
+// const updatedIntegers = integers.map(function (number) {
+//     return number += 1
+// });
+
+//Arrow function, they are more concise.
+const updatedIntegers = integers.map((number) => number += 1); //implicit return
 
 console.log(updatedIntegers);
