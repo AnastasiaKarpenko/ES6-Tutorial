1a6dea193c5b91f123c0f240c50ba02dfc5358f7
Anastasia Karpenko <morganaloca@gmail.com>
2018-12-05 11:45:34 +0200

    learning about let and var

---
+++ src/sandbox.js
@@ -0,0 +1,15 @@
+// function printName() {
+//     var name = "Anastasiia";
+//     console.log(name)
+// }
+//
+// printName();
+
+//var variables are function scope variables
+
+var name = "Anastasiia";
+
+if(name == "Anastasiia") {
+    let fullName = "Anastasiia Karpenko";  //let is a variable for block scope
+    console.log(fullName);
+}
