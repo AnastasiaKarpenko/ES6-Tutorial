803e57d40e0052b87c7dec8ce14e8c4266546849
Anastasia Karpenko <morganaloca@gmail.com>
2018-12-05 12:27:09 +0200

    experimenting with arrow functions and filter ()

--- src/sandbox.js
+++ src/sandbox.js
@@ -5,6 +5,17 @@ const integers = [1, 2, 3];
 // });
 
 //Arrow function, they are more concise.
-const updatedIntegers = integers.map(() => "bob"); //implicit return
+// const updatedIntegers = integers.map(() => "bob"); //implicit return
 
-console.log(updatedIntegers);
+// console.log(updatedIntegers);
+
+const ages = [23, 42, 6, 18];
+
+// const adults = ages.filter(function (age) {
+//     return age > 20
+// });
+
+const adults = ages.filter((age) => age > 20);
+
+console.log(adults);
+
